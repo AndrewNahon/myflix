4b9c7b58a5f439ea20aa62db5354a5e471e008a2
Denholm Nahon <andrew.nahon@gmail.com>
2016-11-29 09:42:01 -0800

    working on mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,10 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  ActionMailer::Base.smtp_settings = {
+  
+end
+
+ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
@@ -27,4 +30,3 @@ Myflix::Application.configure do
   :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
-end
